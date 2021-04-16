a311ffda68125e99419cee01680af64bf4759c3f
jdl9243 <jdl1041238438@outlook.com>
2021-04-16 10:32:21 +0800

    新增标签

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+
+      <div>test</div>
+      
     </div>
     <router-view/>
   </div>
